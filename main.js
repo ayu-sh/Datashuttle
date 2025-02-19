f58f67efa1b01fd956a0caf1d62585e062bc2fe9
ayu-sh <63504172+ayu-sh@users.noreply.github.com>
2025-02-19 23:33:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const navLinks = document.querySelector('.nav-links')
+function toggleMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('hidden');
+  }
+
+const scroll = ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2000
+})
+scroll.reveal('.image-transform', {
+    origin: 'right',
+    interval: 100,
+    delay: 300
+})
+scroll.reveal('.text-transform', {
+    origin: 'left',
+    interval: 100,
+    delay: 300
+})
+scroll.reveal('.services-transform', {
+    origin: 'bottom',
+    interval: 100,
+    delay: 300
+})
+scroll.reveal('.how-transform', {
+    origin: 'top',
+    interval: 100,
+    delay: 300
+})
+
